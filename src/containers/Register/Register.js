7d91192774ed17bcdc1eff4b0c92363d35e8a5ff
Jared Avila <jared.c.avila@gmail.com>
2020-05-19 17:35:42 -0700

    styled register page

--- src/containers/Register/Register.js
+++ src/containers/Register/Register.js
@@ -1,13 +1,46 @@
 import React from "react";
+import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
+import * as styles from "./Register.module.scss";
 
-const Register = () => {
+const Register = (props) => {
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    props.history.push("/register/add_child");
+  };
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/register/add_child">Continue</Link>
+    <div className={styles.container}>
+      <div className={styles.logoContainer}></div>
+      <div className={styles.formContainer}>
+        <h5 className={styles.title}>Account Info</h5>
+        <div className={styles.spacer}></div>
+        <form onSubmit={onSubmitHandler} className={styles.form}>
+          <label htmlFor="fName">First Name</label>
+          <input type="text" placeholder="James" name="fName" />
+          <label htmlFor="lName">Last Name</label>
+          <input type="text" placeholder="Bond" name="lName" />
+          <label htmlFor="email">Email</label>
+          <input type="text" placeholder="You@example.com" name="email" />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            name="password"
+          />
+          <label htmlFor="password2">Confirm Password</label>
+          <input
+            type="password"
+            placeholder="Confirm your password"
+            name="password2"
+          />
+          <input className={styles.button} type="submit" value="Continue" />
+          <p>
+            <Link to="/">Already have an account?</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default Register;
+export default withRouter(Register);
